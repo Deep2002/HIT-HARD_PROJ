392e4eae96609ff28ea64ddd8edb2b82b221aaa7
Deep Parmar <deepparmar@Deeps-MBP.lan>
2021-12-02 15:19:41 -0600

    Added email info

--- ecommerceWeb/settings.py
+++ ecommerceWeb/settings.py
@@ -136,4 +136,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "noreply.hithard@gmail.com"
-EMAIL_HOST_PASSWORD = "1234@Hithard.com"
+EMAIL_HOST_PASSWORD = "**********"
